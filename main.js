61847d6474347355128bf939703e755e82ac2dae
undefined06855 <currentcashew57@gmail.com>
2026-08-06 15:44:34 +0900

    make all requests run in parallel

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 import { Database } from "bun:sqlite"
 import { RateLimiter } from "@rabbit-company/rate-limiter";
 
-const db = new Database(":memory:", { create: true });
+const db = new Database(":memory:");
 db.run(`
 CREATE TABLE IF NOT EXISTS LevelComments (
     id                      INTEGER,
@@ -33,26 +33,24 @@ const limiter = new RateLimiter({
  * @param {string} boomlings
  */
 function boomlingsToSQL(id, boomlings) {
-    if (boomlings == "-1") {
-        return { id, invalid: true }
-    }
+    let [ commentRaw, userRaw ] = boomlings.split(":");
 
-    let [ comment, user ] = boomlings.split(":");
-
-    let commentSplit = comment.split("~");
+    let commentSplit = commentRaw.split("~");
     let commentData = {};
     for (let i = 0; i < commentSplit.length; i += 2) {
         commentData[parseInt(commentSplit[i])] = commentSplit[i + 1];
     }
 
-    let userSplit = user.split("~");
+    let userSplit = userRaw.split("~");
     let userData = {};
     for (let i = 0; i < userSplit.length; i += 2) {
         userData[parseInt(userSplit[i])] = userSplit[i + 1];
     }
 
+    let comment;
+
     try {
-        atob(commentData[2].replaceAll("-", "+").replaceAll("_", "/"))
+        comment = atob(commentData[2].replaceAll("-", "+").replaceAll("_", "/"))
     } catch(err) {
         console.log(commentData);
         console.log(comment);
@@ -63,7 +61,7 @@ function boomlingsToSQL(id, boomlings) {
 
     return {
         id,
-        comment: atob(commentData[2].replaceAll("-", "+").replaceAll("_", "/")),
+        comment,
         likes: parseInt(commentData[4]),
         player_name: userData[1],
         icon_main_color: parseInt(userData[10]),
@@ -102,6 +100,8 @@ let server = Bun.serve({
                     ids.push(int);
                 }
 
+                ids = ids.slice(0, 30);
+
                 let levels = {};
                 let dbComments = [];
                 let gdComments = [];
@@ -112,7 +112,6 @@ let server = Bun.serve({
                             levels[comment.id] = [];
                         }
 
-                        if ("invalid" in comment) continue;
                         levels[comment.id].push(comment);
                     }
                 }
@@ -123,36 +122,47 @@ let server = Bun.serve({
 
                 // ...and for all of the ids we dont already have in the db, fetch them from gd
                 let outdatedIDs = ids.filter(id => !Object.keys(levels).includes(id.toString()));
+                let promises = [];
                 for (let id of outdatedIDs) {
-                    // https://boomlings.dev/endpoints/comments/getGJComments21
-                    let params = new URLSearchParams();
-                    params.append("levelID", id);
-                    params.append("page", "0");
-                    params.append("secret", "Wmfd2893gb7");
-                    params.append("mode", "1"); // most liked
-                    params.append("count", "5");
+                    promises.push(new Promise(async (resolve, reject) => {
+                        // await (async () => { return new Promise(resolve => setTimeout(resolve, i*150))})();
 
-                    let headers = {};
-                    headers["User-Agent"] = "";
-                    headers["Authorization"] = process.env.BOOMLINGS_AUTH ?? "";
+                        // https://boomlings.dev/endpoints/comments/getGJComments21
+                        let params = new URLSearchParams();
+                        params.append("levelID", id);
+                        params.append("page", "0");
+                        params.append("secret", "Wmfd2893gb7");
+                        params.append("mode", "1"); // most liked
+                        params.append("count", "5");
 
-                    let res = await fetch(
-                        process.env.BOOMLINGS_ENDPOINT ?? "https://www.boomlings.com/database/getGJComments21.php", {
-                            headers,
-                            body: params,
-                            method: "POST"
-                        }
-                    );
+                        let headers = {};
+                        headers["User-Agent"] = "";
+                        headers["Authorization"] = process.env.BOOMLINGS_AUTH ?? "";
 
-                    let rawText = await res.text();
-                    if (rawText == "-1") continue;
-                    if (rawText.length < 8) continue;
+                        let res = await fetch(
+                            process.env.BOOMLINGS_ENDPOINT ?? "https://www.boomlings.com/database/getGJComments21.php", {
+                                headers,
+                                body: params,
+                                method: "POST"
+                            }
+                        );
 
-                    let boomlingsComments = rawText.split("|");
-                    gdComments.push(...boomlingsComments.map(boom => boomlingsToSQL(id, boom)));
+                        let rawText = await res.text();
+                        if (rawText == "-1") { reject(rawText); return; }
+                        if (rawText == "too many requests") { reject(rawText); return; }
+                        if (rawText.length < 8) { reject(rawText); return; }
+
+                        let boomlingsComments = rawText.split("|");
+                        resolve(boomlingsComments.map(boom => boomlingsToSQL(id, boom)));
+                    }));
                 }
 
-                collect(gdComments);
+                collect(
+                    (await Promise.allSettled(promises))
+                        .filter(res => res.status == "fulfilled")
+                        .map(res => res.value)
+                        .flat()
+                );
 
                 // and put the gd comments in the db
                 for (let comment of gdComments) {
@@ -174,7 +184,7 @@ let server = Bun.serve({
 
 // every minute, clear old comments
 Bun.cron("* * * * *", () => {
-    db.run("DELETE FROM LevelComments WHERE updated_at < ?", Date.now() - 600000 /* 10 mins */);
+    db.run("DELETE FROM LevelComments WHERE updated_at < ?", Date.now() - 1200000 /* 20 mins */);
 });
 
 console.log(`Comments Preview server on port ${server.port}`);
