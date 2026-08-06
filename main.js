660fec725fb149a6e5a38076785532348d566bc4
undefined06855 <currentcashew57@gmail.com>
2026-08-06 17:55:59 +0900

    log all headers

--- main.js
+++ main.js
@@ -79,12 +79,14 @@ let server = Bun.serve({
 
         "/v1/comments": async req => {
             return new Response(JSON.stringify(await (async () => {
-                let ip = req.headers["CF-Connecting-IP"] ?? server.requestIP(req).address;
-                let res = limiter.check("/comments", ip);
-                if (res.limited) {
-                    console.log(`${ip} is being rate limited!`);
-                    return { error: "You are being rate limited!" };
-                }
+                // let ip = req.headers["CF-Connecting-IP"] ?? server.requestIP(req).address;
+                // let res = limiter.check("/comments", ip);
+                // if (res.limited) {
+                //     console.log(`${ip} is being rate limited!`);
+                //     return { error: "You are being rate limited!" };
+                // }
+
+                console.log(req.headers.toJSON());
 
                 let url = new URL(req.url);
                 if (!url.searchParams.has("levelIDs")) {
