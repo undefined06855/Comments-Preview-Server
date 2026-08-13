b9383a4c88d8a16fed42778697a0c6583f51ca1f
undefined06855 <currentcashew57@gmail.com>
2026-08-13 12:55:23 +0900

    add log when sending umami events

--- umami.js
+++ umami.js
@@ -55,7 +55,7 @@ async function log(name, data={}) {
         }
     });
 
-    if (queue.length >= 50) {
+    if (queue.length >= 100) {
         fetch(
             `${process.env.UMAMI_ENDPOINT}/batch`, {
                 method: "POST",
@@ -65,6 +65,8 @@ async function log(name, data={}) {
                 }
             }
         );
+
+        console.log(`sent over ${queue.length} events`);
 
         queue.length = 0;
     }
